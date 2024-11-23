d0d084d5f92e455ee1304c4ab04b54855395416d
Kevin <kktang@andrew.cmu.edu>
2024-11-23 17:47:47 -0500

    getfavorites function

--- src/lib/api/favorites.ts
+++ src/lib/api/favorites.ts
@@ -1,12 +1,20 @@
 "use server";
+
+import prisma from "../prisma";
+
 /**
  * This is a server-side function.  It will not be included in the client bundle which is good because of the Firebase SDK.
  */
 
 export async function getFavorites(clerkId: string): Promise<string[]>{
+    const User = await prisma.user.findUnique({
+        where: {
+          id: clerkId,
+        },
+      })
     return [];   
 }
 
-export async function addFavorite(appName: string, clerkId: string): Promise<boolean>{
-    return true;
+export async function addFavorite(appName: string, clerkId: string): {
+    return;
 }
